b491f6def1146cfb5cae326065f344e7da3c5310
doomsterinc <doomsterinc1@gmail.com>
2016-03-01 16:21:45 -0300

    addMod2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer(function(req,res){
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-	res.end('Hello World');
+	res.end('Olá Node!!!');
 }).listen(3000);
 
 console.log('Server running at http://localhost:3000/');
